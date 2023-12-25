0ab0e19e2e497f407194e5ccf88d6a6e4d63bb73
SUJAL PANWAR <58047061+sujalpanwarOP@users.noreply.github.com>
2023-12-25 11:08:24 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN ==> not a number 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN ==> not a number 
 
 
 // "33" => 33
@@ -20,7 +20,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false
@@ -31,5 +31,42 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************* Operations **********************//
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3+2);
+// console.log(3*2);
+// console.log(3**2);
+// console.log(3/2);  // doesnt convert to string, 3/2 => 1.5
+// console.log(3%2);
+
+
+let str1 = "sujal"
+let str2 = " panwar"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true + true); ==> 2
+//  console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
