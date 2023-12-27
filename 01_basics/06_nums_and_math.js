fb5750da537868770a5fa2a508dd71225e3f46cc
SUJAL PANWAR <58047061+sujalpanwarOP@users.noreply.github.com>
2023-12-27 07:19:43 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score   = 500
+const balance = new Number(100);
+// console.log( score);
+// console.log( balance);
+// console.log( balance.toString().length);
+// console.log( balance.toFixed(2));
+
+
+const otherNumber =  123.3623
+// console.log(otherNumber.toPrecision(4));  // rounding upto 4 digits 
+
+const hundreds = 1000000
+// console.log( hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-121));
+// console.log(Math.round(121.823132));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(1,2,3,42342,412,));
+// console.log(Math.max(1,34,5,1,2342,13,100));
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1) + min));
