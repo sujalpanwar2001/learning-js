43724d99269e47a34ce58c285ce55078c41388e8
SUJAL PANWAR <58047061+sujalpanwarOP@users.noreply.github.com>
2023-12-27 16:28:39 +0000

    json and object destructure

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,10 +46,30 @@ const users = [
 
 // console.log(users[0].email);
 // console.log(tinderUser);
-console.log(Object.keys(tinderUser)); // returns an array of all keys in the object
-console.log(Object.values(tinderUser)); // returns an array of all values in the object
-console.log(Object.entries(tinderUser)); // returns an array of arrays of key value [ [,,,,,] , [,,,,,], [,,,,,]] 
+// console.log(Object.keys(tinderUser)); // returns an array of all keys in the object
+// console.log(Object.values(tinderUser)); // returns an array of all values in the object
+// console.log(Object.entries(tinderUser)); // returns an array of arrays of key value [ [,,,,,] , [,,,,,], [,,,,,]] 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); // returns boolean value if this property/key exists or not 
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // returns boolean value if this property/key exists or not 
 
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const { courseInstructor: instructor} = course 
+
+console.log(instructor);
+
+// JSON
+// {
+//     "name" : "Sujal",
+//     "coursename" : "chai aur js",
+//     "price" : "free"
+// }
+
+// in the same way we will destructure the json, because its in  kind of an object format 
